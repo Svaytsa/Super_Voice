ae192501e29f87239aa47b6d5f73e83e11eacf30
Svaytsa <svaytsa.laytbackg@gmail.com>
2025-10-01 16:55:47 +0300

    Delete Backend/LocalAI_.server directory

--- Backend/LocalAI_.server/app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""LocalAI FastAPI application package."""
-
-from .main import create_app  # noqa: F401
--- Backend/LocalAI_.server/app/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Router registrations for the LocalAI application."""
-
-from fastapi import APIRouter
-
-api_router = APIRouter()
-
-__all__ = ["api_router"]
